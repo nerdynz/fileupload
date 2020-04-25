6255bb44b78380aca1fdab1e2eed6e3614f818fc
Josh Cave <josh@nerdy.co.nz>
2020-04-25 22:09:07 +1200

    v1

--- fileupload.go
+++ fileupload.go
@@ -1,17 +1,18 @@
 package fileupload
 
 import (
+	"bufio"
 	"bytes"
-	"context"
 	"encoding/json"
+	"errors"
 	"io"
+	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
 
-	"cloud.google.com/go/storage"
 	"github.com/kennygrant/sanitize"
 )
 
@@ -43,44 +44,14 @@ func FromRequestToFile(req *http.Request, path string) (string, string, error) {
 	return filename, fullpath, nil
 }
 
-func FromRequestToGoogleBucket(req *http.Request, bucketName string) (string, string, error) {
-	req.ParseMultipartForm(32)
-	file, handler, err := req.FormFile("file")
-	if err != nil {
-		return "", "", err
-	}
-	defer file.Close()
-
-	ctx := context.Background()
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		// TODO: Handle error.
-	}
-	bkt := client.Bucket(bucketName)
-
-	filename := handler.Filename
-	// fullpath := path + filename
-
-	obj := bkt.Object(filename)
-	if err != nil {
-		return "", "", err
-	}
-	w := obj.NewWriter(ctx)
-	_, err = io.Copy(w, file)
-	if err != nil {
-		return "", "", err
-	}
-
-	if err := w.Close(); err != nil {
-		return "", "", err
-	}
-
-	return filename, bucketName, nil
-}
-
 func FromBuffer(name string, path string, body io.Reader) (string, string, error) {
 	filename := getValidFileName(path, name)
-	fullpath := path + filename
+	return FromBufferNoSanitize(filename, path, body)
+}
+
+func FromBufferNoSanitize(name string, path string, body io.Reader) (string, string, error) {
+	// filename := getValidFileName(path, name)
+	fullpath := path + name
 	f, err := os.OpenFile(fullpath, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		return "", "", err
@@ -91,7 +62,7 @@ func FromBuffer(name string, path string, body io.Reader) (string, string, error
 	if err != nil {
 		return "", "", err
 	}
-	return filename, fullpath, nil
+	return name, fullpath, nil
 }
 
 func getValidFileName(path string, filename string) string {
@@ -145,7 +116,7 @@ func (o *operations) last() *operation {
 	return o.Ops[len(o.Ops)-1]
 }
 
-func ProcessedImageFromRequest(req *http.Request, imageType string, maxWidth int, quality int, convert bool) (*http.Response, error) {
+func ProcessedImageFromRequest(req *http.Request, imageType string, width int, height int, quality int, convert bool) ([]byte, error) {
 	err := req.ParseMultipartForm(32)
 	if err != nil {
 		return nil, err
@@ -155,11 +126,10 @@ func ProcessedImageFromRequest(req *http.Request, imageType string, maxWidth int
 		return nil, err
 	}
 	defer file.Close()
-
-	return ProcessedImage(file, imageType, maxWidth, quality, convert)
+	return ProcessedImage(file, imageType, width, height, quality, convert)
 }
 
-func ProcessedImage(r io.Reader, imageType string, maxWidth int, quality int, convert bool) (*http.Response, error) {
+func ProcessedImage(r io.Reader, imageType string, width int, height int, quality int, convert bool) ([]byte, error) {
 	ops := &operations{}
 
 	originalImageType := "jpg"
@@ -177,46 +147,98 @@ func ProcessedImage(r io.Reader, imageType string, maxWidth int, quality int, co
 	}
 
 	ops.add("fit")
-	// ops.last().addParam("rotate", "0")
-	// ops.last().addParam("background", "255,255,255")
-	ops.last().addParam("width", maxWidth)  //absolute max
-	ops.last().addParam("height", maxWidth) // dont need its ratio based
-	// ops.last().addParam("stripmeta", true) // dont need its ratio based
+	ops.last().addParam("width", width)    //absolute max
+	ops.last().addParam("height", height)  // dont need its ratio based
+	ops.last().addParam("stripmeta", true) // dont need its ratio based
 	ops.last().addParam("quality", quality)
 	// ops.last().addParam("compression", quality)
 	bOps, err := json.Marshal(ops.Ops)
 	if err != nil {
 		return nil, err
 	}
-	endpoint := "https://images.nerdy.co.nz/pipeline?operations=" + url.QueryEscape(string(bOps))
-	// endpoint = "https://images.nerdy.co.nz/fit?width=200&height=200"
+	endpoint := os.Getenv("IMAGE_PROCESSING_ENDPOINT") + "pipeline?operations=" + url.QueryEscape(string(bOps))
 
 	var b bytes.Buffer
 	w := multipart.NewWriter(&b)
 	fw, err := w.CreateFormFile("file", "filename_placeholder."+originalImageType)
 	if err != nil {
 		return nil, err
+		// ctx.ErrorJSON(http.StatusOK, "couldn't create form file ", err)
 	}
 	_, err = io.Copy(fw, r)
 	if err != nil {
+		// ctx.ErrorJSON(http.StatusOK, "failed to copy from reqFile", err)
 		return nil, err
 	}
 	err = w.Close()
 	if err != nil {
+		// ctx.ErrorJSON(http.StatusOK, "failed to copy from reqFile", err)
 		return nil, err
 	}
 
 	req, err := http.NewRequest("POST", endpoint, &b)
 	if err != nil {
+		// ctx.ErrorJSON(http.StatusOK, "failed to copy from reqFile", err)
 		return nil, err
 	}
 	req.Header.Set("Content-Type", w.FormDataContentType())
 
 	client := &http.Client{}
 	res, err := client.Do(req)
+	if err != nil || res.StatusCode != 200 {
+		// ctx.ErrorJSON(http.StatusInternalServerError, "bad request", err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var finalBts bytes.Buffer
+	wr := bufio.NewWriter(&finalBts)
+	// we read from tee reader as it hasn't already done its scan
+	_, err = io.Copy(wr, res.Body)
 	if err != nil {
+		// ctx.ErrorJSON(http.StatusInternalServerError, "Failed to create image", err)
 		return nil, err
 	}
 
-	return res, nil
+	return finalBts.Bytes(), nil
+}
+
+func FromBytes(name string, path string, b []byte) (string, string, error) {
+	return FromBuffer(name, path, bytes.NewReader(b))
+}
+
+func FromBytesNoSanitize(name string, path string, b []byte) (string, string, error) {
+	return FromBufferNoSanitize(name, path, bytes.NewReader(b))
+}
+
+func DownloadFile(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New("download failed for " + url)
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return b, err
+}
+
+func DownloadToFile(url string, filename string, filepath string) (string, string, error) {
+	b, err := DownloadFile(url)
+	if err != nil {
+		return "", "", err
+	}
+	return FromBytes(filename, filepath, b)
+}
+
+func DownloadToFileNoSanitize(url string, filename string, filepath string) (string, string, error) {
+	b, err := DownloadFile(url)
+	if err != nil {
+		return "", "", err
+	}
+	return FromBytesNoSanitize(filename, filepath, b)
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/nerdynz/fileupload
+
+go 1.12
+
+require (
+	github.com/kennygrant/sanitize v1.2.4
+	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd // indirect
+)
